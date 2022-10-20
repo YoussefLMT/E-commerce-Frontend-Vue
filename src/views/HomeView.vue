2fb1aa957d654c2cd0715cccee47f478ce765dd4
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 18:32:40 +0100

    add home content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,22 @@
 <Navbar />
 
 <div class="home">
-    <h1>home</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="home-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quas eum quidem natus, veritatis asperiores facere, voluptate consectetur adipisci ea voluptatem tempore deserunt provident fugit eveniet eos enim? Dolorem, atque.</p>
+                    <button type="button" class="btn btn-primary">Shop Now</button>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="image d-none d-sm-block">
+                    <img src="@/assets/logo.png">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
