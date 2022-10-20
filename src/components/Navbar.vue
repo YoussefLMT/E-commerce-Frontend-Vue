ac03d2375ee0740b09633d24c05586dc23688d8b
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 18:20:13 +0100

    add navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -76,3 +76,141 @@ export default {
 }
 </script>
 
+<style scoped>
+header {
+    background: #fff;
+    z-index: 99;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    transition: .5s ease all;
+    color: white;
+}
+
+header nav {
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    padding: 12px 0;
+    transition: .5s ease all;
+    width: 90%;
+    margin: 0 auto;
+}
+
+header nav ul,
+.link {
+    font-weight: 500;
+    color: black;
+    list-style: none;
+    text-decoration: none;
+}
+
+header nav ul li {
+    padding: 16px;
+    margin-left: 16px;
+}
+
+header nav ul .link {
+    font-size: 20px;
+    transition: .5s ease all;
+    padding-bottom: 14px;
+    border-bottom: 1px solid transparent;
+}
+
+header nav ul .link:hover {
+    color: #4F46E5;
+    border-color: #4F46E5;
+}
+
+.logo h2 {
+    color: black;
+    font-weight: 600;
+    font-size: 35px;
+    cursor: pointer;
+}
+
+.logo a {
+    text-decoration: none;
+}
+
+.logo .txt {
+    color: #4F46E5;
+}
+
+header .logo {
+    display: flex;
+    align-items: center;
+}
+
+header .logo img {
+    width: 60px;
+}
+
+header .navigation {
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: flex-end;
+}
+
+header .icon {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 24px;
+    height: 100%;
+}
+
+header .icon i {
+    cursor: pointer;
+    font-size: 24px;
+    transition: .8s ease all;
+    color: black;
+}
+
+header .icon-active {
+    transform: rotate(180deg);
+}
+
+header .dropdown-nav {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    max-width: 250px;
+    background-color: white;
+    top: 0;
+    left: 0;
+}
+
+header .dropdown-nav li {
+    margin-left: 0;
+}
+
+header .dropdown-nav li .link {
+    color: black;
+}
+
+header .mobile-nav-enter-active,
+.mobile-nav-leave-active {
+    transition: .8s ease all;
+}
+
+header .mobile-nav-enter-from,
+.mobile-nav-leave-to {
+    transform: translateX(-250px);
+}
+
+header .mobile-nav-enter-to {
+    transform: translateX(0);
+}
+
+@media(min-width: 1140px) {
+    header nav {
+        max-width: 1140px;
+    }
+}
+</style>
