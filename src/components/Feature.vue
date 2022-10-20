921c803cf4079f4aab2bd52ef53c7c13d18d5ae9
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 21:33:15 +0100

    add feature component

---
+++ src/components/Feature.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="box">
+    <div class="imgbox">
+        <img :src="image">
+    </div>
+    <div class="text">
+        <h3>{{ name }}</h3>
+        <p>{{ description }}</p>
+    </div>
+</div>
+</template>
