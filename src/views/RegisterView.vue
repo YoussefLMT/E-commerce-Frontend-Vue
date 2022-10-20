c07d81cecaa64d6bbe2a384108d54d8676090440
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 20:01:35 +0100

    add register view

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,26 @@
+<template>
+<NavBar />
+
+<div class="auth-content">
+    <form>
+        <h2 class="form-title">Register</h2>
+        <div>
+            <label>Name</label>
+            <input type="text" class="text-input">
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email" class="text-input">
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" class="text-input">
+        </div>
+        <div>
+            <button type="button" class="btn btn-big">Register</button>
+        </div>
+        <p>Or <router-link to="/login">Login</router-link>
+        </p>
+    </form>
+</div>
+</template>
