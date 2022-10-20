aeb3fb935c4efee1f414764223d26e236963823c
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 18:53:30 +0100

    add login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+<NavBar />
+
+<div class="auth-content">
+    <form>
+        <h2 class="form-title">Login</h2>
+        <div>
+            <label>Email</label>
+            <input type="email" class="text-input">
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" class="text-input">
+        </div>
+        <div>
+            <button type="button" class="btn btn-big">Login</button>
+        </div>
+        <p>Or <router-link to="/register">Register</router-link>
+        </p>
+    </form>
+</div>
+</template>
