bcc0c338e96b6e345c05b1e1fa7e1ee8c6035c59
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 21:56:52 +0100

    add category component

---
+++ src/components/Category.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="box">
+    <div class="imgbox">
+        <img :src="image">
+    </div>
+    <div class="text">
+        <h3>{{ name }}</h3>
+    </div>
+</div>
+</template>
