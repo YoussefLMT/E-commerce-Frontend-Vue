7e2a2eeb71aa2a2bdf280f5477af2072d9191928
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 18:55:40 +0100

    add script for login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -21,6 +21,16 @@
 </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
+
 <style scoped>
 .auth-content {
     width: 30%;
