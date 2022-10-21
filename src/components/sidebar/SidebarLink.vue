fd1b68ff7f7878e8bb15777cafe98ce848f646cc
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 11:41:22 +0100

    add sidebarlink component

---
+++ src/components/sidebar/SidebarLink.vue
@@ -0,0 +1,10 @@
+<template>
+<router-link :to="to" class="link" :class="{ active: isActive }">
+    <i class="icon" :class="icon" />
+    <transition name="fade">
+        <span v-if="!collapsed">
+            <slot />
+        </span>
+    </transition>
+</router-link>
+</template>
