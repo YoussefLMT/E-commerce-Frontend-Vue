844611a2ed3a0fcfa80b4bfff1c784fc650081a7
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 18:54:16 +0100

    add some style

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -4,7 +4,7 @@
 <div :style="{ 'margin-left': sidebarWidth }" class="products">
     <h1>Products</h1>
 
-     <div class="card pr" style="width: 1100px;">
+    <div class="card pr" style="width: 1100px;">
         <div class="card-header">
             Products Managment
 
@@ -62,5 +62,17 @@ export default {
 .products {
     padding: 20px;
     transition: 0.3s ease;
+}
+
+.pr {
+    margin: 50px auto;
+}
+
+img {
+    width: 50px;
+}
+
+.btn-add {
+    float: right
 }
 </style>
