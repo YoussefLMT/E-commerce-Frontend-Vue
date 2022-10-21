b1e55f31a02b2f0b4f6c069671f86628759542b2
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 11:38:30 +0100

    add sidebar component

---
+++ src/components/sidebar/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="sidebar" :style="{ width: sidebarWidth }">
+
+    <div class="links">
+
+    </div>
+
+    <button @click="logOut" class="logout-btn">Log Out</button>
+
+    <span class="collapse-icon" :class="{ 'rotate-180': collapsed }" @click="toggleSidebar">
+        <i class="fas fa-angle-double-left" />
+    </span>
+</div>
+</template>
