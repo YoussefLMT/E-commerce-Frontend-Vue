c4fe99af062d50987871f0e6938434034486ebb0
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 20:14:23 +0100

    add product component

---
+++ src/components/Product.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="box">
+    <div class="imgbox">
+        <img :src="image">
+    </div>
+    <div class="text">
+        <h3>{{ name }}</h3>
+        <p>{{ price }} DH</p>
+        <router-link :to="{ name: 'mealDetails', params: {id: id }}" class="btn-show">Show</router-link>
+    </div>
+</div>
+</template>
