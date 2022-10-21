990727fe2ac0abb614aee2802405e15e636a6728
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 21:52:31 +0100

    add ProductDetailsModule

---
+++ src/store/ProductDetailsModule.js
@@ -0,0 +1,41 @@
+import axiosInstance from '../axios'
+
+const ProductDetailsModule = {
+    namespaced: true,
+    state: {
+        product: [],
+        loading: false
+    },
+    getters: {
+        product(state) {
+            return state.product
+        },
+
+        loading(state) {
+            return state.loading
+        }
+    },
+    mutations: {
+        getProduct(state, product) {
+            state.product = product
+        },
+
+        setLoading(state, loading) {
+            state.loading = loading
+        },
+    },
+    actions: {
+        getMeal({ commit }, id) {
+            commit('setLoading', true)
+
+            axiosInstance.get(`/get-product/${id}`)
+                .then(response => {
+                    commit('getProduct', response.data.product)
+                    commit('setLoading', false)
+                })
+                .catch(error => console.log(error))
+        },
+    },
+}
+
+export default ProductDetailsModule
