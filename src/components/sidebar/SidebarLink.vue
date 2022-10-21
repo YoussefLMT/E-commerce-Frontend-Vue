6d5a8800916d579ee4836a0c0b8eb793fd1e0866
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 11:41:56 +0100

    add sidebarlink script

--- src/components/sidebar/SidebarLink.vue
+++ src/components/sidebar/SidebarLink.vue
@@ -8,3 +8,21 @@
     </transition>
 </router-link>
 </template>
+
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { collapsed } from './sidebarState'
+
+export default {
+  props: {
+    to: { type: String, required: true },
+    icon: { type: String, required: true }
+  },
+  setup(props) {
+    const route = useRoute()
+    const isActive = computed(() => route.path === props.to)
+    return { isActive, collapsed }
+  }
+}
+</script>
