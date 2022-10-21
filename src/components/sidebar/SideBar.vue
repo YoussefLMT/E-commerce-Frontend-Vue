0ec572bc106341bbcb766f946c0604ca9fd4730e
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 11:40:00 +0100

    add sidebar style

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -37,3 +37,58 @@ export default {
     },
 }
 </script>
+
+<style>
+:root {
+    --sidebar-bg-color: #1e293b;
+    --sidebar-item-hover: #64748b;
+    --sidebar-item-active: #334155;
+}
+</style><style scoped>
+.sidebar {
+    color: white;
+    background-color: var(--sidebar-bg-color);
+    float: left;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    padding: 0.5em;
+    transition: 0.3s ease;
+    display: flex;
+    flex-direction: column;
+}
+
+.sidebar h1 {
+    height: 2.5em;
+}
+
+.collapse-icon {
+    position: absolute;
+    bottom: 0;
+    padding: 0.75em;
+    color: rgba(255, 255, 255, 0.7);
+    transition: 0.2s linear;
+    cursor: pointer;
+}
+
+.rotate-180 {
+    transform: rotate(180deg);
+    transition: 0.2s linear;
+}
+
+.logout-btn {
+    margin: 350px 10px 200px 10px;
+    border: 0;
+    padding: 10px;
+    border-radius: 10px;
+    background-color: #2980b9;
+    font-weight: bold;
+    color: white;
+}
+
+.links {
+    margin-top: 20px;
+}
+</style>
