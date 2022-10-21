e9736326915d28648f007633f9e1e6a07ed5b4e8
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 11:47:51 +0100

    add not found view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occured, Requested page not found!
+                </div>
+                <div class="error-actions">
+                    <router-link to="/" class="btn btn-primary btn-lg">Take Me Home</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
