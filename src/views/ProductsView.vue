b9d77e9fe12360bfca4fc562e4e591660f68cbee
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 18:57:24 +0100

    add add new product modal

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -8,6 +8,10 @@
         <div class="card-header">
             Products Managment
 
+            <!-- Button trigger add products modal -->
+            <button type="button" class="btn btn-primary btn-add" data-bs-toggle="modal" data-bs-target="#addProductModal">
+                Add Product
+            </button>
         </div>
         <div class="card-body">
             <table class="table">
@@ -35,6 +39,54 @@
                     </tr>
                 </tbody>
             </table>
+        </div>
+    </div>
+
+    <!--Add Product Modal -->
+    <div class="modal fade" id="addMealModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add New Product</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="fname" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="fname" >
+                        </div>
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Price</label>
+                            <input type="text" class="form-control" id="price" >
+                        </div>
+                         <div class="mb-3">
+                            <label for="quantity" class="form-label">Quantity</label>
+                            <input type="text" class="form-control" id="quantity" >
+                        </div>
+                        <div class="mb-3">
+                            <label for="quantity" class="form-label">Category</label>
+                            <select class="form-select">
+                                <option value="tshirt">T-shirt</option>
+                                <option value="jacket">Jacket</option>
+                                <option value="pants">Pants</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea class="form-control" id="description" rows="3"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Image</label>
+                            <input class="form-control" type="file" id="image">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
