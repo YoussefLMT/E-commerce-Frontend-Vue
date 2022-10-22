2cd5942b4a3cb3dcd4a02c5ff5567d9bb2c5d71b
YoussefLMT <mcyoussef324@gmail.com>
2022-10-22 12:31:51 +0100

    call getProducts ction

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -11,11 +11,23 @@
 <script>
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer.vue'
+import store from '@/store'
 
 export default {
     components: {
         Navbar,
         Footer,
+    },
+    mounted() {
+        store.dispatch('products/getProducts')
+    },
+    computed: {
+        products() {
+            return store.getters['products/products']
+        },
+        loading() {
+            return store.getters['products/loading']
+        },
     },
 }
 </script>
