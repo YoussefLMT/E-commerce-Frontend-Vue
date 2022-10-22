fe9e08e0cd32d9047c77535c7681c20862c06dd7
YoussefLMT <mcyoussef324@gmail.com>
2022-10-22 18:27:38 +0100

    add cart view

---
+++ src/views/CartView.vue
@@ -0,0 +1,23 @@
+<template>
+<Navbar />
+
+<div class="footer">
+    <Footer />
+</div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer.vue'
+
+export default {
+    components: {
+        Navbar,
+        Footer,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
