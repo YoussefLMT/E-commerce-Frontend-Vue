80408e9503dca4edfd034f15d4f8567ab6209f7f
YoussefLMT <mcyoussef324@gmail.com>
2022-10-22 18:30:08 +0100

    add CartModule

---
+++ src/store/CartModule.js
@@ -0,0 +1,41 @@
+import axiosInstance from '../axios'
+
+const CartModule = {
+    namespaced: true,
+    state: {
+        cart_products: [],
+        loading: false
+    },
+    getters: {
+        cartProducts(state) {
+            return state.cart_products
+        },
+
+        loading(state) {
+            return state.loading
+        }
+    },
+    mutations: {
+        getCartProducts(state, cart_products) {
+            state.cart_products = cart_products
+        },
+
+        setLoading(state, loading) {
+            state.loading = loading
+        },
+    },
+    actions: {
+        getCartProducts({ commit }) {
+            commit('setLoading', true)
+
+            axiosInstance.get('/get-cart-meals')
+                .then(response => {
+                    commit('getCartProducts', response.data.cart_products)
+                    commit('setLoading', false)
+                })
+                .catch(error => console.log(error))
+        },
+    },
+}
+
+export default CartModule
