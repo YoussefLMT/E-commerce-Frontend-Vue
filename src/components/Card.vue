4c68a88831484d5c18fa0f12b9412c56c239ef34
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 21:08:00 +0100

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<template>
+<div id="root">
+    <div class="container mt-4">
+        <div class="c-dashboardInfo">
+            <div class="wrap">
+                <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title"><i :class="icon"></i>{{ title }}</h4><span class="hind-font caption-12 c-dashboardInfo__count">{{ count }}</span>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
