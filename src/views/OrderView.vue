fe44c090622194a6f3764a698b9ca1021285d2e6
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 19:20:23 +0100

    add order form

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,8 +1,38 @@
 <template>
 <Navbar />
 
-<div>
-
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th scope="col">Total Amount</th>
+                        <td>34534534 DH</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6 mt-5 mx-auto">
+            <form>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="address" v-model="address">
+                </div>
+                <div class="mb-3">
+                    <label for="city" class="form-label">City</label>
+                    <input type="text" class="form-control" id="city" v-model="city">
+                </div>
+                <div class="mb-3">
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="text" class="form-control" id="phone" v-model="phone">
+                </div>
+                <button type="button" @click="placeOrder" class="btn">Order</button>
+            </form>
+        </div>
+    </div>
 </div>
 
 <div class="footer">
