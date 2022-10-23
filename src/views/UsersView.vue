ace11ecda446aa10609fd513983cfc46ad16012b
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 12:52:50 +0100

    display users

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -24,12 +24,15 @@
                         <th scope="col">actions</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">6</th>
-                        <td>gygyg</td>
-                        <td>yuff</td>
-                        <td>gyug</td>
+                 <div v-if="loading" class="spinner">
+                    <h3>Loading...</h3>
+                </div>
+                <tbody v-else>
+                    <tr v-for="user in users" :key="user.id">
+                        <th scope="row">{{ user.id }}</th>
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>{{ user.role }}</td>
                     </tr>
                 </tbody>
             </table>
