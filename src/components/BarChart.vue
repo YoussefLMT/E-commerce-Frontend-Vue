740499257889e4cb645f1f6d786a5fb48fb79996
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 21:19:35 +0100

    add barchart component

---
+++ src/components/BarChart.vue
@@ -0,0 +1,3 @@
+<template>
+<Bar :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId" :dataset-id-key="datasetIdKey" :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width" :height="height" />
+</template>
