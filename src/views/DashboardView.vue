4de4d64b77ab3c111f42838a2dcb3b93778f9ae0
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 21:09:47 +0100

    add count data

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -19,6 +19,10 @@ export default {
     data() {
         return {
             sidebarWidth,
+            ordersCount: '',
+            mealsCount: '',
+            usersCount: '',
+            income: ''
         }
     },
 }
