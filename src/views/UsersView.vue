ca2e3aa6116d7034694a1ff5e3d40b20c6639d6a
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 12:45:56 +0100

    add users table

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,8 +1,36 @@
 <template>
 <Sidebar />
 
-<div :style="{ 'margin-left': sidebarWidth }" class="products">
+<div :style="{ 'margin-left': sidebarWidth }" class="users">
     <h1>Users</h1>
+
+    <div class="card us" style="width: 900px;">
+        <div class="card-header">
+            Users Managment
+
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">name</th>
+                        <th scope="col">email</th>
+                        <th scope="col">role</th>
+                        <th scope="col">actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td>gygyg</td>
+                        <td>yuff</td>
+                        <td>gyug</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 </div>
 </template>
@@ -29,5 +57,13 @@ export default {
 .users {
     padding: 20px;
     transition: 0.3s ease;
+}
+
+.us {
+    margin: 50px auto;
+}
+
+.btn-add {
+    float: right
 }
 </style>
