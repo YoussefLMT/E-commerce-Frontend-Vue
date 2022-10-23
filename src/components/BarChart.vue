709f8ba4883f9e1992e8c5481da8d0d04812a710
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 21:20:20 +0100

    add barchar script

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,3 +1,73 @@
 <template>
 <Bar :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId" :dataset-id-key="datasetIdKey" :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width" :height="height" />
 </template>
+
+<script>
+import {
+    Bar
+} from 'vue-chartjs'
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+} from 'chart.js'
+import axiosInstance from '@/axios'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+export default {
+    name: 'BarChart',
+    components: {
+        Bar
+    },
+    props: {
+        chartId: {
+            type: String,
+            default: 'bar-chart'
+        },
+        datasetIdKey: {
+            type: String,
+            default: 'label'
+        },
+        width: {
+            type: Number,
+            default: 220
+        },
+        height: {
+            type: Number,
+            default: 150
+        },
+        cssClasses: {
+            default: '',
+            type: String
+        },
+        styles: {
+            type: Object,
+            default: () => {}
+        },
+        plugins: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    data() {
+        return {
+            chartData: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [{
+                    label: 'Orders Per Month',
+                    backgroundColor: '#1851A8',
+                    data: []
+                }]
+            },
+            chartOptions: {
+                responsive: true
+            },
+        }
+    },
+
+}
+</script>
