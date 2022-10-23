272607ad329578e60e4ec5a5d06c1bdd9eedab9d
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 20:06:31 +0100

    display orders

--- src/views/MyOrdersView.vue
+++ src/views/MyOrdersView.vue
@@ -1,6 +1,30 @@
 <template>
 <Navbar />
 
+<div class="container mt-5">
+    <h3 class="text-center">My Orders</h3><br><br>
+    <div v-if="loading" class="text-center mb-5">
+        <Circle />
+    </div>
+    <div class="row" v-else v-for="order in userOrders" :key="order.id">
+        <div class="col-sm-3">
+            <div class="card mb-3" style="width: 18rem;">
+                <img class="card-img-top" :src="'http://127.0.0.1:8000/' + order.image">
+                <div class="card-body">
+                    <h5 class="card-title">{{ order.name }}</h5>
+                    <p class="card-text">{{ order.price }} DH</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 ml-5">
+            <p>Address: {{ order.address }}</p>
+            <p>City: {{ order.city }}</p>
+            <p>Phone: {{ order.phone }}</p>
+            <p class="badge tt text-bg-warning">Status: {{ order.status }}</p>
+        </div>
+    </div>
+</div>
+
 <div class="footer">
     <Footer />
 </div>
