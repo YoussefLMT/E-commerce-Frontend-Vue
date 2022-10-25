848d43aa2c0128365b3b8dd16045d6c19174d092
YoussefLMT <mcyoussef324@gmail.com>
2022-10-25 18:32:47 +0100

    add navbar to about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
+<Navbar />
+
+<div class="about">
     <h1>This is an about page</h1>
-  </div>
+</div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+    components: {
+        Navbar
+    },
+}
+</script>
