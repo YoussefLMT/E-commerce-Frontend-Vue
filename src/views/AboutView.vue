4cb7abbfb4d5511d090be5da504c724b2d061370
YoussefLMT <mcyoussef324@gmail.com>
2022-10-25 18:34:36 +0100

    add about content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,17 @@
 <Navbar />
 
 <div class="about">
-    <h1>This is an about page</h1>
+    <div class="row">
+        <div class="col50">
+            <h1 class="title-txt">About <span>Us</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reprehenderit, aut repellat, dolorem illo, voluptatem minima sapiente praesentium earum possimus nemo debitis est sunt? Cumque minus sit quis aliquam veniam!<br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, deserunt maxime consequuntur dolores quae unde vitae, incidunt quisquam quibusdam voluptates sed iure ad, fugit provident optio nisi praesentium et doloremque? </p>
+        </div>
+        <div class="col50">
+            <div class="imgbox">
+                <img src="@/assets/undraw_shopping_app_flsj.svg">
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
