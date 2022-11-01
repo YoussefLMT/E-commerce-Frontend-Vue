4efadc2fa55a84c1b6447edd2d59a6873fb919ff
YoussefLMT <mcyoussef324@gmail.com>
2022-11-01 17:48:27 +0100

    prevent adding quantity more than product quantity

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -1,143 +1,161 @@
 <template>
-<Navbar />
+  <Navbar />
 
-<div class="container">
+  <div class="container">
     <div v-if="loading" class="text-center mb-5">
-        <Circle />
+      <Circle />
     </div>
     <div v-else class="row d-flex justify-content-center">
-        <div class="col-md-4">
-            <img class="details-img" :src="'http://127.0.0.1:8000/' + product.image">
+      <div class="col-md-4">
+        <img
+          class="details-img"
+          :src="'http://127.0.0.1:8000/' + product.image"
+        />
+      </div>
+      <div class="col-md-4">
+        <router-link to="/">Go Back</router-link><br /><br />
+        <h5>{{ product.name }}</h5>
+        <p><span class="title">Price:</span> {{ product.price }}DH</p>
+        <p><span class="title">Description:</span> {{ product.description }}</p>
+        <p><span class="title">Quantity:</span> {{ product.quantity }}</p>
+        <div class="quantity-toggle">
+          <button @click="decrement()">&mdash;</button>
+          <input type="text" :value="quantity" readonly />
+          <button @click="increment()" :disabled="product.quantity === '0'">
+            &#xff0b;
+          </button>
         </div>
-        <div class="col-md-4">
-            <router-link to="/">Go Back</router-link><br><br>
-            <h5>{{ product.name }}</h5>
-            <p><span class="title">Price:</span> {{ product.price }}DH</p>
-            <p><span class="title">Description:</span> {{ product.description }}</p>
-            <p><span class="title">Quantity:</span> {{ product.quantity}}</p>
-            <div class="quantity-toggle">
-                <button @click="decrement()">&mdash;</button>
-                <input type="text" :value="quantity" readonly>
-                <button @click="increment()" :disabled="product.quantity === '0'">&#xff0b;</button>
-            </div><br>
-            <button @click="addTocart" class="btn">Add Product To Cart</button>
-        </div>
+        <br />
+        <button @click="addTocart" class="btn">Add Product To Cart</button>
+      </div>
     </div>
-</div>
+  </div>
 
-<div class="footer">
+  <div class="footer">
     <Footer />
-</div>
+  </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer.vue'
-import store from '@/store'
-import Circle from 'vue-loading-spinner/src/components/Circle'
-import axiosInstance from '../axios'
-import Swal from 'sweetalert2'
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer.vue";
+import store from "@/store";
+import Circle from "vue-loading-spinner/src/components/Circle";
+import axiosInstance from "../axios";
+import Swal from "sweetalert2";
 
 export default {
-    components: {
-        Navbar,
-        Footer,
-        Circle
+  components: {
+    Navbar,
+    Footer,
+    Circle,
+  },
+  data() {
+    return {
+      quantity: 1,
+    };
+  },
+  mounted() {
+    store.dispatch("productDetails/getProduct", this.$route.params.id);
+  },
+  computed: {
+    product() {
+      return store.getters["productDetails/product"];
     },
-    data() {
-        return {
-            quantity: 1
-        }
+    loading() {
+      return store.getters["productDetails/loading"];
     },
-    mounted() {
-        store.dispatch('productDetails/getProduct', this.$route.params.id)
+  },
+  methods: {
+    increment() {
+      // if (this.product.quantity === 1) {
+      //     this.quantity = 1
+      // } else {
+      this.quantity++;
+      // this.product.quantity--
+      // }
     },
-    computed: {
-        product() {
-            return store.getters['productDetails/product']
-        },
-        loading() {
-            return store.getters['productDetails/loading']
-        }
-    },
-    methods: {
-        increment() {
-            if (this.product.quantity === 1) {
-                this.quantity = 1
-            } else {
-                this.quantity++
-                this.product.quantity--
-            }
-        },
 
-        decrement() {
-            if (this.quantity === 1) {
-                this.quantity = 1
-            } else {
-                this.quantity--
-                this.product.quantity++
-            }
-        },
+    decrement() {
+      if (this.quantity === 1) {
+        this.quantity = 1;
+      } else {
+        this.quantity--;
+        // this.product.quantity++;
+      }
+    },
 
-        async addTocart() {
-            if (!localStorage.getItem('token')) {
-                this.$router.push('/login')
-                const Toast = Swal.mixin({
-                    toast: true,
-                    position: 'top-end',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true,
-                    didOpen: (toast) => {
-                        toast.addEventListener('mouseenter', Swal.stopTimer)
-                        toast.addEventListener('mouseleave', Swal.resumeTimer)
-                    }
-                })
-                Toast.fire({
-                    icon: 'error',
-                    title: 'Please login first'
-                })
-            } else {
-                const response = await axiosInstance.post(`add-to-cart/${this.$route.params.id}`, {
-                    quantity: this.quantity
-                })
-                if (response.data.status === 406) {
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 2000,
-                        timerProgressBar: true,
-                        didOpen: (toast) => {
-                            toast.addEventListener('mouseenter', Swal.stopTimer)
-                            toast.addEventListener('mouseleave', Swal.resumeTimer)
-                        }
-                    })
-                    Toast.fire({
-                        icon: 'error',
-                        title: response.data.message
-                    })
-                } else if (response.data.status === 200) {
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 2000,
-                        timerProgressBar: true,
-                        didOpen: (toast) => {
-                            toast.addEventListener('mouseenter', Swal.stopTimer)
-                            toast.addEventListener('mouseleave', Swal.resumeTimer)
-                        }
-                    })
-                    Toast.fire({
-                        icon: 'success',
-                        title: response.data.message
-                    })
-                }
+    async addTocart() {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener("mouseenter", Swal.stopTimer);
+          toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+      });
+
+      if (!localStorage.getItem("token")) {
+        this.$router.push("/login");
+
+        Toast.fire({
+          icon: "error",
+          title: "Please login first",
+        });
+      } else {
+        if (this.quantity > this.product.quantity) {
+          Toast.fire({
+            icon: "error",
+            title: "Quantity you choose is more what we have!",
+          });
+        } else {
+          const response = await axiosInstance.post(
+            `add-to-cart/${this.$route.params.id}`,
+            {
+              quantity: this.quantity,
             }
+          );
+          if (response.data.status === 406) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 2000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+              },
+            });
+            Toast.fire({
+              icon: "error",
+              title: response.data.message,
+            });
+          } else if (response.data.status === 200) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 2000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+              },
+            });
+            Toast.fire({
+              icon: "success",
+              title: response.data.message,
+            });
+          }
         }
-    }
-}
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -149,65 +167,65 @@ export default {
 } */
 
 .details-img {
-    height: 300px;
+  height: 300px;
 }
 
 .btn {
-    font-size: 13px;
-    font-weight: 600;
-    border-radius: 7px;
-    color: #fff;
-    background: #EAB800;
-    display: inline-block;
-    padding: 10px 30px;
-    text-transform: uppercase;
-    text-decoration: none;
-    letter-spacing: 2px;
+  font-size: 13px;
+  font-weight: 600;
+  border-radius: 7px;
+  color: #fff;
+  background: #eab800;
+  display: inline-block;
+  padding: 10px 30px;
+  text-transform: uppercase;
+  text-decoration: none;
+  letter-spacing: 2px;
 }
 
 .btn:hover {
-    background: #C19800;
+  background: #c19800;
 }
 
 .navbar {
-    background-color: blue;
-    padding: 45px 0px;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px 0px;
+  background-color: blue;
+  padding: 45px 0px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px 0px;
 }
 
 .container {
-    margin-top: 180px;
+  margin-top: 180px;
 }
 
 p {
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .title {
-    font-weight: bold;
+  font-weight: bold;
 }
 
 .quantity-toggle {
-    display: flex;
+  display: flex;
 }
 
 .quantity-toggle input {
-    border: 0;
-    border-top: 2px solid #ddd;
-    border-bottom: 2px solid #ddd;
-    width: 2.5rem;
-    text-align: center;
-    padding: 0 .5rem;
+  border: 0;
+  border-top: 2px solid #ddd;
+  border-bottom: 2px solid #ddd;
+  width: 2.5rem;
+  text-align: center;
+  padding: 0 0.5rem;
 }
 
 .quantity-toggle button {
-    border: 0;
-    padding: .2rem;
-    background: #EAB800;
-    color: white;
-    font-size: 1rem;
-    font-weight: bold;
-    cursor: pointer;
+  border: 0;
+  padding: 0.2rem;
+  background: #eab800;
+  color: white;
+  font-size: 1rem;
+  font-weight: bold;
+  cursor: pointer;
 }
 
 /* @media only screen and (max-width: 800px) {
